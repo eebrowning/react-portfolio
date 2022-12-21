1ede076fb3655b41875e523b6dc54a06f24cbbc2
eebrowning <eebrowning828@gmail.com>
2022-12-21 10:07:13 -0700

    added to skills section

--- src/Components/skills/Skills.js
+++ src/Components/skills/Skills.js
@@ -4,60 +4,69 @@ const Skills = () => {
 
 
     return (
+        <div>
+            <div id="skills">
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/react_logo.png" alt="react"
+                        className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/redux_logo.png" alt="redux"
+                        className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/js_logo.png" alt="javascript"
+                        className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/kisspng-python-programming-language-computer-programming-5aefaba2926b57.9208708715256564825997.png"
+                        alt="python" className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/html_logo.png" alt="html"
+                        className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/css_logo.png" alt="css"
+                        className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/node_logo.png" alt="node"
+                        className="img-fluid" />
+                </div>
 
-        <div id="skills">
-
-
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/react_logo.png" alt="react"
-                    className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/redux_logo.png" alt="redux"
-                    className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/js_logo.png" alt="javascript"
-                    className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/kisspng-python-programming-language-computer-programming-5aefaba2926b57.9208708715256564825997.png"
-                    alt="python" className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/html_logo.png" alt="html"
-                    className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/css_logo.png" alt="css"
-                    className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/node_logo.png" alt="node"
-                    className="img-fluid" />
-            </div>
-
-            <div className="custom-skill-logo">
-                <img src="https://i.imgur.com/pdZdQEG.png" alt="flask"
-                    className="img-fluid" />
-            </div>
+                <div className="custom-skill-logo">
+                    <img src="https://i.imgur.com/pdZdQEG.png" alt="flask"
+                        className="img-fluid" />
+                </div>
 
 
-            <div className="custom-skill-logo">
-                <img src="https://www.mementotech.in/assets/images/icons/express.png" alt="express"
-                    className="img-fluid" />
+                <div className="custom-skill-logo">
+                    <img src="https://www.mementotech.in/assets/images/icons/express.png" alt="express"
+                        className="img-fluid" />
+                </div>
+
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/postgresql_logo.png"
+                        alt="postgresql" className="img-fluid" />
+                </div>
+                <div className="custom-skill-logo">
+                    <img src="https://www.gaborkurucz.com/assets/images/custom/logos/aws_logo.png" alt="aws"
+                        className="img-fluid" />
+                </div>
+
             </div>
 
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/postgresql_logo.png"
-                    alt="postgresql" className="img-fluid" />
-            </div>
-            <div className="custom-skill-logo">
-                <img src="https://www.gaborkurucz.com/assets/images/custom/logos/aws_logo.png" alt="aws"
-                    className="img-fluid" />
-            </div>
 
-        </div>
+            <div className="skills-details" >
+
+                <img class='stats' src="https://github-readme-stats.vercel.app/api?username=eebrowning&count_private=true&show_icons=true" />
+                <img class='stats' src="https://github-readme-stats.vercel.app/api/top-langs/?username=eebrowning" />
+
+
+            </div>
+        </div >
+
     )
 }
 
