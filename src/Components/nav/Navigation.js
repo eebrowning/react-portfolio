a9a106c6dfb1ece946f067d6162908ef0c11241e
eebrowning <eebrowning828@gmail.com>
2023-09-26 15:14:28 -0400

    working on overhaul

--- src/Components/nav/Navigation.js
+++ src/Components/nav/Navigation.js
@@ -3,17 +3,19 @@ import './navigation.css'
 
 const Navigation = () => {
     return (
-        <nav id="nav">
+        <>
 
-            <a href="#projects-box-outer" class="button scrolly">Projects</a>
-            <a href="#skills" class="button scrolly">Skills</a>
-            <a href="#contact-outer" class="button scrolly">Contact</a>
-            <a href="https://docs.google.com/document/d/1GwiBjkQBFZ4wADwn3tSXkjsasqWoWKCiVMGD-kj33ko/edit"
-                id="resume-nav" className="button scrolly">My Resume</a>
+            <nav id="nav">
+                <a href="#projects-box-outer" className="button scrolly">Projects</a>
+                <a href="#skills" className="button scrolly">Skills</a>
+                <a href="#contact-outer" className="button scrolly">Contact</a>
+                <a href="https://docs.google.com/document/d/1GwiBjkQBFZ4wADwn3tSXkjsasqWoWKCiVMGD-kj33ko/edit"
+                    id="resume-nav" className="button scrolly">My Resume</a>
 
 
-        </nav>
+            </nav>
 
+        </>
     )
 }
 
